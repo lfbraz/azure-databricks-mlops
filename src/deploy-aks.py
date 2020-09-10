1eb7151dc2b1bf6fc55384cfec3b989b0d952f86
Luiz Braz <luiz.braz@live.com>
2020-09-10 16:19:04 -0300

    Alter deployment mode (remove deprecated methods)

--- src/deploy-aks.py
+++ src/deploy-aks.py
@@ -3,19 +3,23 @@
 
 # COMMAND ----------
 
+# Experiment parameters
+experiment_name = '/churn-prediction'
+run_name = 'mlops-train'
+
+model_name = 'churn-model'
+
 # Workspace parameters
 workspace_name = dbutils.secrets.get(scope = "azure-key-vault", key = "workspace-name")
 workspace_location = dbutils.secrets.get(scope = "azure-key-vault", key = "location")
 resource_group = dbutils.secrets.get(scope = "azure-key-vault", key = "resource-group")
 subscription_id = dbutils.secrets.get(scope = "azure-key-vault", key = "subscription-id")
 
-# Image parameters
-image_name = 'churn-model-image'
-
 # Prod instance parameters
 endpoint_name_prod = 'api-churn-prod'
 aks_target = 'aks-cluster-1'
 
+model_uri = get_model_uri(experiment_name, run_name)
 workspace = get_workspace(workspace_name, workspace_location, resource_group, subscription_id)
 
-deploy_aks(workspace, endpoint_name_prod, image_name, aks_target)
+deploy_aks(workspace, model_uri, endpoint_name_prod, model_name, aks_target)
